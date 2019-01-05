369dae97ad6cd482496e7d99328d3487b972e40a
Max <mjann@ucsc.edu>
2019-01-05 12:13:46 -0800

    changing elements by class name

---
+++ static/javascript/nerves.js
@@ -0,0 +1,12 @@
+// this is js
+function myfunction(){
+    // alert("idk. ,anm");
+    var x = document.getElementById("x");
+    var y = document.getElementsByClassName("gold");
+    x.innerHTML = "pikachu";
+    for(var i =0; i < y.length; i++){
+        y[i].innerHTML = "charizard";
+    }
+    // y[0].innerHTML = "charizard";
+}
+// alert('pbj');   // it's working  !! this is so much fun
